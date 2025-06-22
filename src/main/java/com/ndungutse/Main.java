3e5b84558e69f4023309fb168b9ddcb2bc446af1
ericndungutse <dav.ndungutse@gmail.com>
2025-06-22 17:15:41 +0200

    feat: log thread pool status and task tracker analytics

--- src/main/java/com/ndungutse/Main.java
+++ src/main/java/com/ndungutse/Main.java
@@ -2,6 +2,7 @@ package com.ndungutse;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import com.ndungutse.consumer.TaskConsumer;
 import com.ndungutse.producer.TaskProducer;
@@ -12,9 +13,6 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         TaskQueue taskQueue = new TaskQueue();
 
-        // Start the monitor thread
-        Thread monitorThread = new Thread(new MonitorThread(taskQueue), "Monitor-Thread");
-
         Thread producer1 = new Thread(new TaskProducer(taskQueue, "Producer-1", 2));
         Thread producer2 = new Thread(new TaskProducer(taskQueue, "Producer-2", 2));
         Thread producer3 = new Thread(new TaskProducer(taskQueue, "Producer-3", 2));
@@ -24,18 +22,20 @@ public class Main {
         producer2.start();
         producer3.start();
 
-        monitorThread.start();
-
         producer1.join();
         producer2.join();
         producer3.join();
 
         // Consumers
-        ExecutorService workerPool = Executors.newFixedThreadPool(3);
+        ThreadPoolExecutor workerPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(3);
         for (int i = 1; i <= 3; i++) {
             workerPool.submit(new TaskConsumer(taskQueue));
         }
 
+        // Start the monitor thread
+        Thread monitorThread = new Thread(new MonitorThread(taskQueue, workerPool), "Monitor-Thread");
+
+        monitorThread.start();
         workerPool.shutdown();
 
     }
